53a951b24eadbe63a43d2e867bd505ce2d5c8d54
cicovic-andrija <cicovic.andrija@gmail.com>
2020-05-01 19:06:25 +0200

    Initial ui layout

---
+++ front.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    console.log("aaa")
+}
+
+function saveTextToFile(text, filename, type) {
+    var file = new Blob([text], {type: type});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"), url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);
+        }, 0);
+    }
+}
